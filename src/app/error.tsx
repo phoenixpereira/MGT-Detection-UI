4807aa678dfbe90fcc4686a0b4eebcaadbd551bc
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2024-04-28 18:09:43 +0930

    Fix code formatting

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,38 +1,42 @@
-'use client'; // Error components must be Client Components
+"use client"; // Error components must be Client Components
 
-import * as React from 'react';
-import { RiAlarmWarningFill } from 'react-icons/ri';
+import * as React from "react";
+import { RiAlarmWarningFill } from "react-icons/ri";
 
-import TextButton from '@/components/buttons/TextButton';
+import TextButton from "@/components/buttons/TextButton";
 
 export default function Error({
-  error,
-  reset,
+      error,
+      reset
 }: {
-  error: Error & { digest?: string };
-  reset: () => void;
+      error: Error & { digest?: string };
+      reset: () => void;
 }) {
-  React.useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.error(error);
-  }, [error]);
+      React.useEffect(() => {
+            // eslint-disable-next-line no-console
+            console.error(error);
+      }, [error]);
 
-  return (
-    <main>
-      <section className='bg-white'>
-        <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
-          <RiAlarmWarningFill
-            size={60}
-            className='drop-shadow-glow animate-flicker text-red-500'
-          />
-          <h1 className='mt-8 text-4xl md:text-6xl'>
-            Oops, something went wrong!
-          </h1>
-          <TextButton variant='basic' onClick={reset} className='mt-4'>
-            Try again
-          </TextButton>
-        </div>
-      </section>
-    </main>
-  );
+      return (
+            <main>
+                  <section className="bg-white">
+                        <div className="layout flex min-h-screen flex-col items-center justify-center text-center text-black">
+                              <RiAlarmWarningFill
+                                    size={60}
+                                    className="drop-shadow-glow animate-flicker text-red-500"
+                              />
+                              <h1 className="mt-8 text-4xl md:text-6xl">
+                                    Oops, something went wrong!
+                              </h1>
+                              <TextButton
+                                    variant="basic"
+                                    onClick={reset}
+                                    className="mt-4"
+                              >
+                                    Try again
+                              </TextButton>
+                        </div>
+                  </section>
+            </main>
+      );
 }
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,17 +4,17 @@
  * Imported through src/app/page.tsx
  * @see https://x.com/mattpocockuk/status/1760991147793449396
  */
-import { z } from 'zod';
+import { z } from "zod";
 
 const envVariables = z.object({
-  NEXT_PUBLIC_SHOW_LOGGER: z.enum(['true', 'false']).optional(),
+      NEXT_PUBLIC_SHOW_LOGGER: z.enum(["true", "false"]).optional()
 });
 
 envVariables.parse(process.env);
 
 declare global {
-  namespace NodeJS {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface ProcessEnv extends z.infer<typeof envVariables> {}
-  }
+      namespace NodeJS {
+            // eslint-disable-next-line @typescript-eslint/no-empty-interface
+            interface ProcessEnv extends z.infer<typeof envVariables> {}
+      }
 }
