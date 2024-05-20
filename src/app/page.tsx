a4f1f3c66af4f2ce90c370634f6dca8b726e5c5a
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2024-05-20 13:31:41 +0930

    Conditionally show upload text and results

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,36 +21,41 @@ export default function Home() {
 
   const [model1Result, setModel1Result] = useState<number | null>(null);
   const [model2Result, setModel2Result] = useState<number | null>(null);
-  const [model3Result, setModel3Result] = useState<number | null>(null);
+    const [model3Result, setModel3Result] = useState<number | null>(null);
+    const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
 
   return (
-    <div className="container mx-auto p-8">
-      <h1 className="text-3xl font-bold mb-4">MGT Detector</h1>
-      <div className="grid grid-cols-1 gap-4">
-        <OverallResults
-          machineGeneratedProbability={
-            overallResult.machineGeneratedProbability
-          }
-          generatedText={overallResult.generatedText}
-          model1Result={model1Result}
-          model2Result={model2Result}
-          model3Result={model3Result}
-        />
-        <FileUpload
-          setOverallResult={setOverallResult}
-          setModel1Result={setModel1Result}
-          setModel2Result={setModel2Result}
-          setModel3Result={setModel3Result}
-          highlightedChunks={overallResult.highlightedChunks}
-          textChunks={overallResult.textChunks}
-        />
-        {overallResult.generatedText && (
-          <HighlightedText
-            textChunks={overallResult.textChunks}
-            highlightedChunks={overallResult.highlightedChunks}
-          />
-        )}
-      </div>
-    </div>
+        <div className="container mx-auto p-8">
+              <h1 className="text-3xl font-bold mb-4">MGT Detector</h1>
+              <div className="grid grid-cols-1 gap-4">
+                    {isSubmitted && (
+                          <OverallResults
+                                machineGeneratedProbability={
+                                      overallResult.machineGeneratedProbability
+                                }
+                                generatedText={overallResult.generatedText}
+                                model1Result={model1Result}
+                                model2Result={model2Result}
+                                model3Result={model3Result}
+                          />
+                    )}
+                    <FileUpload
+                          setOverallResult={setOverallResult}
+                          setModel1Result={setModel1Result}
+                          setModel2Result={setModel2Result}
+                          setModel3Result={setModel3Result}
+                          highlightedChunks={overallResult.highlightedChunks}
+                          textChunks={overallResult.textChunks}
+                          isSubmitted={isSubmitted}
+                          setIsSubmitted={setIsSubmitted}
+                    />
+                    {overallResult.generatedText && (
+                          <HighlightedText
+                                textChunks={overallResult.textChunks}
+                                highlightedChunks={overallResult.highlightedChunks}
+                          />
+                    )}
+              </div>
+        </div>
   );
 }
--- src/components/TextAnalyser.tsx
+++ src/components/TextAnalyser.tsx
@@ -2,20 +2,21 @@ import React, { useState } from "react";
 import "react-circular-progressbar/dist/styles.css";
 
 interface TextAnalyserProps {
-  text: string;
-  highlightedChunks: any[];
-  textChunks: any[];
-  setOverallResult: React.Dispatch<
-    React.SetStateAction<{
-      machineGeneratedProbability: number | null;
-      generatedText: string | null;
+      text: string;
       highlightedChunks: any[];
       textChunks: any[];
-    }>
-  >;
-  setModel1Result: React.Dispatch<React.SetStateAction<number | null>>;
-  setModel2Result: React.Dispatch<React.SetStateAction<number | null>>;
-  setModel3Result: React.Dispatch<React.SetStateAction<number | null>>;
+      setOverallResult: React.Dispatch<
+            React.SetStateAction<{
+                  machineGeneratedProbability: number | null;
+                  generatedText: string | null;
+                  highlightedChunks: any[];
+                  textChunks: any[];
+            }>
+      >;
+      setModel1Result: React.Dispatch<React.SetStateAction<number | null>>;
+      setModel2Result: React.Dispatch<React.SetStateAction<number | null>>;
+      setModel3Result: React.Dispatch<React.SetStateAction<number | null>>;
+      setIsSubmitted: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const TextAnalyser: React.FunctionComponent<TextAnalyserProps> = ({
@@ -23,9 +24,10 @@ const TextAnalyser: React.FunctionComponent<TextAnalyserProps> = ({
   setOverallResult,
   setModel1Result,
   setModel2Result,
-  setModel3Result,
+    setModel3Result,
+  setIsSubmitted,
 }) => {
-  const [loading, setLoading] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
 
   const CHUNK_SIZE = 705; // Maximum chunk size accepted by the model
 
@@ -48,7 +50,8 @@ const TextAnalyser: React.FunctionComponent<TextAnalyserProps> = ({
   ];
 
   const analyseText = async () => {
-    setLoading(true);
+      setLoading(true);
+      setIsSubmitted(true);
     try {
       let textChunks = [];
       let allScores = new Array(models.length).fill(0);
