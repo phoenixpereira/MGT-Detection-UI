96360e2d9e60699f8c15cf6eca3cba91a942f59a
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2024-05-28 15:04:00 +0930

    Update next.config.js and tsconfig.json

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    // set 'fs' to an empty module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-    config.resolve.fallback = {
-      fs: false,
-    };
-    config.module.rules.push({
-      test: /\.node/,
-      use: "raw-loader",
-    });
-    return config;
-  },
+      distDir: "out",
+      webpack: (config) => {
+            // set 'fs' to an empty module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+            config.resolve.fallback = {
+                  fs: false
+            };
+            config.module.rules.push({
+                  test: /\.node/,
+                  use: "raw-loader"
+            });
+            return config;
+      }
 };
 
 module.exports = nextConfig;
